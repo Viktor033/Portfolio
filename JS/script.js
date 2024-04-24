0e471b21eb0aa25e30c5f5984ba8e0a039dd2015
Victor033 <vk.developer.web@gmail.com>
2024-04-24 12:08:02 -0300

    agregado de animaciones al texto

--- JS/script.js
+++ JS/script.js
@@ -35,3 +35,15 @@ window.onscroll =  () => {
     navbar.classList.remove('active');
 
 }
+
+/*===================== scroll reveal  ===================*/
+
+ScrollReveal({
+     reset: true, 
+     distance: '80px',
+     duration: 2000,
+     delay: 200,
+});
+
+ScrollReveal().reveal('.inicio-contenedor, .heading', { origin: 'top' });
+ScrollReveal().reveal('.inicio-img, .Servicios-contenedor, .Portfolio-box, .Contacto form', { origin: 'bottom' });
